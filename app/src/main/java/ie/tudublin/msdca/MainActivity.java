5029870ba1b1cc3231f5c2d593733c352dbcd4c9
EvanLi1116 <c21414026@mytudublin.ie>
2023-12-13 11:08:57 +0000

    1

--- app/src/main/java/ie/tudublin/msdca/MainActivity.java
+++ app/src/main/java/ie/tudublin/msdca/MainActivity.java
@@ -12,12 +12,16 @@ import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.widget.TextView;
 
 import ie.tudublin.msdca.database.DAOs.UserDAO;
 import ie.tudublin.msdca.database.UserDatabase;
 import ie.tudublin.msdca.database.entities.User;
 
 public class MainActivity extends AppCompatActivity {
+    private String user = "Log In";
+    private String welcome = "Welcome! Please login to continue.";
+    public UserDAO userDAO;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,5 +32,14 @@ public class MainActivity extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN
         );
+
+        // Database setup
+        userDAO = Room.databaseBuilder(this, UserDatabase.class, "user-database")
+                .allowMainThreadQueries().build().getUserDAO();
+
+        TextView welcomeText = findViewById(R.id.welcomeText);
+        TextView userText = findViewById(R.id.userText);
+        userText.setText(user);
+        welcomeText.setText(welcome);
     }
 }
